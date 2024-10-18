36b949cc2607b32f96c747649d84ce2d0bd3b209
devidaskale480@gmail.com <devidaskale480@gmail.com>
2024-10-18 19:47:31 +0530

    remove bug from update product & event component

--- src/app/course-details/course-details.component.ts
+++ src/app/course-details/course-details.component.ts
@@ -363,7 +363,7 @@ export class CourseDetailsComponent implements OnInit {
         next: (response) => {
           // console.log(alert("Success"),response);
           Swal.fire('Congratulation', 'Welcome to Ximbo! <br> Were thrilled to have you join our community of esteemed trainers, coaches, and educators. Ximbo is designed to empower you with the tools and resources needed to deliver exceptional training and create impactful learning experiences. <br> You Have Register successfully!', 'success');
-          this.route.navigate(['/cart'])
+          // this.route.navigate(['/cart'])
         },
         error: (error) => {
           // console.log(alert("Error"),error);
